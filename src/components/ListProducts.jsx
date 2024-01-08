91949ee762418075d790eb6b1abfe3a1343e6b96
ArmanDJsan <sc.armand@gmail.com>
2024-01-08 18:21:44 -0400

    Crear-tabla

--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -1,19 +1,42 @@
 import { useState, useEffect } from "react";
-import Product from "./product";
+import './ListProducts.css'
 
-    const ListProducts = () =>{
+const ListProducts = () => {
     const [products, setProducts] = useState([])
-    useEffect(() =>{
+    useEffect(() => {
         fetch("https://fakestoreapi.com/products")
-        .then(response => response.json())
-        .then(data => setProducts(data))
-    
-    },[]);
-    return(
+            .then(response => response.json())
+            .then(data => setProducts(data))
+
+    }, []);
+    return (
         <>
-            {products.map((element) => 
-                <Product product={element} key={element.id} />
-                )}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Product</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product) =>
+                        <tr key={product.id} className="table-row">
+                            <td>
+                                <div className="product-box" >
+                                    <img src={product.image} alt="imagen del producto" height={120} width={120} />
+                                </div>
+                            </td>
+                            <td>{product.title}</td>
+                            <td>
+                                <p>{product.price}$</p>
+                                <button>buy</button>
+                            </td>
+                        </tr>
+                    )}
+                </tbody >
+            </table>
         </>
     )
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,23 +1,43 @@
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
 
-return (
-<div>
-    <div className="product-img">
-        <img src={product.image} height={200} width={200} />
-    </div>
-    <div className="product-info">
-        <div className="product-text">
-            <h1>{product.title}</h1>
-            <p> {product.description} </p> 
+    return (
+
+        <div className="wrapper">
+            <table>
+                <thead>
+                    <th>Product</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                </thead>
+                <tbody>
+                    <tr key={product.id}>
+                    <td><img src={product.image} alt="imagen del producto" /></td>
+                    <td>{product.description}</td>
+                    <td>{product.price}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+
         </div>
-        <div className="product-price-btn">
-            <p><span>{product.price}</span>$</p>
-            <button type="button">buy now</button>
-        </div>
-    </div>
-</div>
-)
+    )
 }
 
 export default Product
+
+
+
+{/* <div className="product-img">
+<img src={product.image} height={200} width={200} />
+</div>
+<div className="product-info">
+<div className="product-text">
+    <h1>{product.title}</h1>
+    <p> {product.description} </p>
+</div>
+<div className="product-price-btn">
+    <p><span>{product.price}</span>$</p>
+    <button type="button">buy now</button>
+</div>
+</div> */}
